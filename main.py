bdb3690eeed63f3275c20c7d155fc096fc5b397b
Danilo <danilogmoura@gmail.com>
2024-04-17 03:10:46 -0300

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ generative_model = ModelFactory().new_model()
 
 def wait_for_enter():
     console.input(
-        prompt="Pressione ENTER para continar...",
+        prompt='Pressione ENTER para continar...',
         password=True
     )
 
@@ -20,49 +20,49 @@ def generate_question(topic):
 
 def run_quiz():
     score = 0
-    continue_quiz = ""
+    continue_quiz = ''
 
-    topico = Prompt.ask("Qual é o tópico do quiz?")
+    topic = Prompt.ask('Qual é o tópico do quiz?')
 
-    while continue_quiz.lower() != "n":
-        with console.status("Processando", spinner="dots"):
-            question = generate_question(topico)
+    while continue_quiz.lower() != 'n':
+        with console.status('Processando'):
+            question = generate_question(topic)
 
-        options = question["options"]
+        options = question['options']
         console.clear()
-        console.print(f"[bold]{question["statement"]}")
+        console.print(f'[bold]{question["statement"]}')
 
         for i, option in enumerate(options, start=1):
-            console.print(f"{i}) {option}")
+            console.print(f'{i}) {option}')
 
         answer_index = int(
             Prompt.ask(
-                prompt="Opção",
-                choices=["1", "2", "3", "4"])
+                prompt='Opção',
+                choices=['1', '2', '3', '4'])
         ) - 1
 
         user_answer = options[answer_index]
-        correct_answer = question["correct"]
+        correct_answer = question['correct']
 
         if user_answer == correct_answer:
             score += 1
-            console.print(f"[green]Você acertou! Agora você tem {score} pontos")
+            console.print(f'[green]Você acertou! Agora você tem {score} pontos')
         else:
-            console.print(f"[red]Você errou! Você continua {score} pontos")
-            console.print(f"A resposta certa é [yellow]{user_answer}")
+            console.print(f'[red]Você errou! Você continua {score} pontos')
+            console.print(f'A resposta certa é [yellow]{user_answer}')
 
         continue_quiz = Prompt.ask(
-            prompt="Desejesa continuar? ",
-            choices=["S", "n"],
-            default="S"
+            prompt='Desejesa continuar? ',
+            choices=['S', 'n'],
+            default='S'
         )
 
 
 def main():
     console.clear()
 
-    title = "[bold yellow]Quiz GEMINI[/bold yellow]"
-    console.print(f"Bem vindo ao {title}")
+    title = '[bold yellow]Quiz GEMINI[/bold yellow]'
+    console.print(f'Bem vindo ao {title}')
 
     run_quiz()
 
